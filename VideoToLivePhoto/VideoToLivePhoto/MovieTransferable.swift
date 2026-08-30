6854dd116d79f1379dd5c71b0880327b6feae68d
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-08-30 22:31:54 +0500

    Add movie picker transferable

---
+++ VideoToLivePhoto/VideoToLivePhoto/MovieTransferable.swift
@@ -0,0 +1,22 @@
+import Foundation
+import CoreTransferable
+import UniformTypeIdentifiers
+
+struct PickedMovie: Transferable {
+    let url: URL
+
+    static var transferRepresentation: some TransferRepresentation {
+        FileRepresentation(contentType: .movie) { movie in
+            SentTransferredFile(movie.url)
+        } importing: { received in
+            let ext = received.file.pathExtension.isEmpty ? "mov" : received.file.pathExtension
+            let destination = FileManager.default.temporaryDirectory
+                .appendingPathComponent("picked-\(UUID().uuidString)")
+                .appendingPathExtension(ext)
+
+            try? FileManager.default.removeItem(at: destination)
+            try FileManager.default.copyItem(at: received.file, to: destination)
+            return PickedMovie(url: destination)
+        }
+    }
+}
